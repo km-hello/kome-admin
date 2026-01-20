c870559b066aa49662c2076f555a1ced2f0fb0b5
kongmeng <km@km-o.com>
2026-01-20 17:24:39 +0800

    feat: integrate vue-sonner and setup router & Pinia
    - Added `vue-sonner` for toast notifications with rich colors and a top-right position.
    - Integrated `Pinia` for state management and registered it in the main app.
    - Configured Vue Router and registered it for navigation handling.
    - Updated global styles by importing `vue-sonner` and custom CSS.
    
    ---
    
    - 添加了 `vue-sonner` 用于弹出消息通知，支持丰富的颜色和右上角位置。
    - 集成了 `Pinia` 用于状态管理，并在主应用中注册。
    - 配置了 Vue 路由器，并完成注册以处理导航。
    - 更新了全局样式，导入了 `vue-sonner` 和自定义 CSS。

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 引入全局样式
 import './style.css'
+import 'vue-sonner/style.css'
+import {createPinia} from "pinia";
+import router from "@/router";
 
-createApp(App).mount('#app')
+// 创建 Vue 应用实例
+const app = createApp(App)
+
+// 注册插件
+app.use(createPinia())  // 状态管理
+app.use(router)         // 路由
+
+// 挂载应用
+app.mount('#app')
+
